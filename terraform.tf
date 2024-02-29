fb144c5df36a28250af23a85466e960ea572212a
Manikandan Prakash <140643626+M95kandan@users.noreply.github.com>
2024-03-01 02:02:08 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,102 +1,119 @@
-
-
-
 provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_security_group" "web_sg" {
-  name        = "web_sg"
-  description = "Security group for web instances"
+resource "aws_security_group" "k8s-sg" {
+  name        = "k8s-sg"
+  description = "Allow all traffic"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-resource "aws_security_group_rule" "web_ingress" {
-  security_group_id = aws_security_group.web_sg.id
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-}
+resource "aws_security_group" "docker-qa-sg" {
+  name        = "docker-qa-sg"
+  description = "Allow traffic for Docker and QA"
 
-resource "aws_security_group_rule" "http_ingress" {
-  security_group_id = aws_security_group.web_sg.id
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "k8s_master" {
-  ami                    = "ami-05a5bb48beb785bf1"
-  instance_type          = "t2.medium"
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
-  key_name               = "jenkins-slave-key"
+  ami           = "ami-0187337106779cdf8"
+  instance_type = "t2.medium"
+  key_name      = "jenkins-slave-key"
+
+  vpc_security_group_ids = [aws_security_group.k8s-sg.id]
+
   tags = {
     Name = "k8s_master"
   }
 }
 
-resource "aws_instance" "k8s_slave" {
-  count                  = 3
-  ami                    = "ami-05a5bb48beb785bf1"
-  instance_type          = "t2.medium"
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
-  key_name               = "jenkins-slave-key"
+resource "aws_instance" "k8s_node" {
+  count         = 3
+  ami           = "ami-0187337106779cdf8"
+  instance_type = "t2.medium"
+  key_name      = "jenkins-slave-key"
+
+  vpc_security_group_ids = [aws_security_group.k8s-sg.id]
+
   tags = {
-    Name = "k8s_slave-${count.index + 1}"
+    Name = "k8s_node-${count.index + 1}"
   }
 }
 
-resource "aws_instance" "building_docker" {
-  ami                    = "ami-0e670eb768a5fc3d4"
-  instance_type          = "t2.medium"
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
-  key_name               = "jenkins-slave-key"
+resource "aws_instance" "docker_host" {
+  ami           = "ami-0e670eb768a5fc3d4"
+  instance_type = "t2.medium"
+  key_name      = "jenkins-slave-key"
+
+  vpc_security_group_ids = [aws_security_group.docker-qa-sg.id]
+
   tags = {
-    Name = "Building docker ec-2"
+    Name = "docker_host"
   }
 }
 
-resource "aws_instance" "qa_testing" {
-  ami                    = "ami-0e670eb768a5fc3d4"
-  instance_type          = "t2.small"
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
-  key_name               = "jenkins-slave-key"
+resource "aws_instance" "qa_host" {
+  ami           = "ami-0e670eb768a5fc3d4"
+  instance_type = "t2.small"
+  key_name      = "jenkins-slave-key"
+
+  vpc_security_group_ids = [aws_security_group.docker-qa-sg.id]
+
   tags = {
-    Name = "QA-ec2"
+    Name = "qa_host"
   }
 }
 
-resource "null_resource" "ansible_inventory" {
-  triggers = {
-    update_trigger = timestamp()
-  }
-
-  depends_on = [
-    aws_instance.k8s_master,
-    aws_instance.k8s_slave,
-    aws_instance.building_docker,
-    aws_instance.qa_testing,
-  ]
-
+resource "null_resource" "generate_inventory" {
   provisioner "local-exec" {
-    command = <<-EOT
-      echo "[k8s_master]" > inventory
-      echo "${aws_instance.k8s_master.public_ip}" >> inventory
-      echo "[k8s_nodes]" >> inventory
-      echo "${join("\n", aws_instance.k8s_slave[*].public_ip)}" >> inventory
-      echo "[git_and_docker]" >> inventory
-      echo "${aws_instance.building_docker.public_ip}" >> inventory
-      echo "[qa_testing]" >> inventory
-      echo "${aws_instance.qa_testing.public_ip}" >> inventory
-    EOT
+    command = <<EOF
+cat <<'EOT' > inventory
+[k8s_master]
+${aws_instance.k8s_master.public_ip}
+
+[k8s_nodes]
+%{ for ip in aws_instance.k8s_node.*.public_ip ~}
+${ip}
+%{ endfor ~}
+
+[docker_host]
+${aws_instance.docker_host.public_ip}
+
+[qa_host]
+${aws_instance.qa_host.public_ip}
+EOT
+EOF
   }
 }
-resource "null_resource" "cleanUpInventory" {
+
+resource "null_resource" "delete_inventory" {
   provisioner "local-exec" {
-    when    = destroy
+    when = destroy
     command = "rm -f inventory"
   }
 }
